b64961086734a1b532a469dfe02a292f5b4e7429
radoslav39 <radoslav84@mail.bg>
2017-01-17 16:19:38 +0200

    ReCommit

--- TestRepoProject/TestRepoProject/TestRepo.cs
+++ TestRepoProject/TestRepoProject/TestRepo.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestRepoProject
 {
-    class Program
+    class TestRepo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hi from GitHub");
         }
     }
 }
